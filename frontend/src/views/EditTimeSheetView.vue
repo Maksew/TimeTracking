c8c917fcd1a35e6e7a6af87e73dcb1bdfb26e544
EyaRejeb <93796426+EyaRejeb@users.noreply.github.com>
2025-03-14 02:03:20 +0100

    TimeSheet front

---
+++ frontend/src/views/EditTimeSheetView.vue
@@ -0,0 +1,15 @@
+<template>
+    <!-- Main Content: Timesheet Form -->
+  <div class="timeSheet" >
+    <TimeSheet />
+  </div>
+</template>
+
+<script setup>
+import TimeSheet from '@/components/timesheet/TimeSheet.vue'
+</script>
+
+<style scoped>
+.timeSheet {
+  padding: 0 16px;
+}</style>
