4b4468bf5c34ff51c830350b5aafac00e5d49f21
PERRIER <m1x6me@gmail.com>
2025-03-22 16:47:47 +0100

    Basic timesheet service added

---
+++ backend/src/main/java/isis/projet/backend/service/TimeSheetService.java
@@ -0,0 +1,34 @@
+package isis.projet.backend.service;
+
+import isis.projet.backend.entity.TimeSheet;
+import isis.projet.backend.entity.User;
+import isis.projet.backend.repository.TimeSheetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TimeSheetService {
+
+    @Autowired
+    private TimeSheetRepository timeSheetRepository;
+
+    public List<TimeSheet> getAllTimeSheetsByUserId(Integer userId) {
+        return timeSheetRepository.findByUserId(userId);
+    }
+
+    public Optional<TimeSheet> getTimeSheetById(Integer id) {
+        return timeSheetRepository.findById(id);
+    }
+
+    public TimeSheet createTimeSheet(TimeSheet timeSheet, User user) {
+        timeSheet.setUser(user);
+        if (timeSheet.getEntryDate() == null) {
+            timeSheet.setEntryDate(LocalDate.now());
+        }
+        return timeSheetRepository.save(timeSheet);
+    }
+}
