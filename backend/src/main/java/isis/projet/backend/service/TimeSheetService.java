15ee76940e9e0ebf8917f7647b493dd32de7bd5f
EyaRejeb <eyarejeb02@gmail.com>
2025-04-02 04:03:05 +0200

    Fixed Add Task to TimeSheet endpoint

--- backend/src/main/java/isis/projet/backend/service/TimeSheetService.java
+++ backend/src/main/java/isis/projet/backend/service/TimeSheetService.java
@@ -5,6 +5,7 @@ import isis.projet.backend.repository.TimeSheetRepository;
 import isis.projet.backend.repository.TimeSheetShareGroupRepository;
 import isis.projet.backend.repository.TimeSheetShareUserRepository;
 import isis.projet.backend.repository.TimeSheetTaskRepository;
+import isis.projet.backend.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +30,10 @@ public class TimeSheetService {
 
     @Autowired
     private TimeSheetShareGroupRepository timeSheetShareGroupRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
 
     /**
      * Récupère toutes les feuilles de temps d'un utilisateur
@@ -106,12 +111,28 @@ public class TimeSheetService {
      * @return Tâche ajoutée à la feuille de temps
      */
     public TimeSheetTask addTaskToTimeSheet(Integer timeSheetId, Integer taskId, Integer duration) {
-        TimeSheetTask timeSheetTask = new TimeSheetTask();
-        timeSheetTask.setTimeSheetId(timeSheetId);
-        timeSheetTask.setTaskId(taskId);
-        timeSheetTask.setDuration(duration);
+        // Retrieve the timesheet from the database
+        TimeSheet timeSheet = timeSheetRepository.findById(timeSheetId)
+                .orElseThrow(() -> new RuntimeException("TimeSheet introuvable"));
+
+        // Retrieve the task from the database
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+
+
+        // Create the TimeSheetTask entity by setting the composite key and relationships
+        TimeSheetTask timeSheetTask = TimeSheetTask.builder()
+                .timeSheetId(timeSheet.getId())
+                .taskId(task.getId())
+                .duration(duration)
+                .timeSheet(timeSheet)
+                .task(task)
+                .build();
+
+        // Save the association entity
         return timeSheetTaskRepository.save(timeSheetTask);
     }
+
 
     /**
      * Récupère les feuilles de temps par date
