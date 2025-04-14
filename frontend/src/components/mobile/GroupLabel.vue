ec26a79e0c38d28c0001a52e0734c8d13a0e8db8
EyaRejeb <eyarejeb02@gmail.com>
2025-04-14 03:32:27 +0200

    GroupLabel component
    Add a component to display the current group label along with the count of filtered timesheets.

---
+++ frontend/src/components/mobile/GroupLabel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="selectedGroup || filteredCount > 0" class="group-label mb-3">
+    <span class="font-weight-medium" :class="{'personal-label': selectedGroup === 'personnel'}">
+      {{ currentGroupLabel }}
+      <span v-if="filteredCount > 0">
+        - {{ filteredCount }} {{ filteredCount > 1 ? 'feuilles' : 'feuille' }}
+      </span>
+    </span>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+  selectedGroup: null,
+  currentGroupLabel: String,
+  filteredCount: Number,
+});
+</script>
