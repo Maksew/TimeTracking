315d218ba5f03fcb5bcf20c9eea5d5749b82319d
EyaRejeb <eyarejeb02@gmail.com>
2025-04-14 03:35:12 +0200

    SuccessNotification component
    A component to display asynchronous success messages after task updates.

---
+++ frontend/src/components/mobile/SuccessNotification.vue
@@ -0,0 +1,15 @@
+<template>
+  <transition name="fade">
+    <div class="success-notification">
+      <v-icon class="mr-2" color="white">mdi-check-circle</v-icon>
+      {{ message }}
+    </div>
+  </transition>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+  message: String,
+});
+</script>
