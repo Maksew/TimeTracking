90a821e6ee37cff296c32d7f764ded3bf8d1c3ee
EyaRejeb <eyarejeb02@gmail.com>
2025-04-14 03:30:14 +0200

    Add TimerDisplay component
    a component to display the task timer and handle start/stop actions in the main view.

---
+++ frontend/src/components/mobile/TimerDisplay.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="d-flex justify-center align-center py-4">
+    <div class="text-h3 font-weight-bold timer">
+      {{ formattedTimerDisplay }}
+    </div>
+    <template v-if="!timerRunning">
+      <v-btn
+        icon
+        class="ml-4 play-btn"
+        color="green"
+        :disabled="!selectedTask"
+        @click="$emit('startTimer')"
+      >
+        <v-icon>mdi-play</v-icon>
+        <v-tooltip activator="parent" location="bottom">Démarrer</v-tooltip>
+      </v-btn>
+    </template>
+    <template v-else>
+      <v-btn
+        icon
+        class="ml-4 stop-btn"
+        color="red"
+        @click="$emit('stopTimer')"
+      >
+        <v-icon>mdi-stop</v-icon>
+        <v-tooltip activator="parent" location="bottom">Arrêter</v-tooltip>
+      </v-btn>
+    </template>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+  timerRunning: Boolean,
+  formattedTimerDisplay: String,
+  selectedTask: Object,
+});
+</script>
