611a4b1aa4bc07a30b6a936c88027216ac7fae78
EyaRejeb <eyarejeb02@gmail.com>
2025-04-14 18:05:13 +0200

    trying to fix deployment error

--- backend/src/main/java/isis/projet/backend/config/WebConfig.java
+++ backend/src/main/java/isis/projet/backend/config/WebConfig.java
@@ -2,34 +2,15 @@ package isis.projet.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Apply CORS to API endpoints
-                .allowedOrigins("*") // Allow frontend requests
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/public/");
-    }
-
-    @Controller
-    static class RouterController {
-        @RequestMapping(value = "/{path:[^\\.]*}")
-        public String redirectToIndex() {
-            return "forward:/index.html";
-        }
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 }
